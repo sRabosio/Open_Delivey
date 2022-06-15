ac3daa76e94912ecd138ff135d7058ca2fcd1d4b
Verderio_Riccardo <103107952+Rivi02@users.noreply.github.com>
2022-06-15 15:13:28 +0200

    Add files via upload

---
+++ OpenDelivery/src/main/java/it/opendelivey/demo/OpenDeliveryApplication.java
@@ -0,0 +1,13 @@
+package it.opendelivey.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenDeliveryApplication.class, args);
+	}
+
+}
