ce351a773e59ccfe0f43f857762dbef4f988e89c
Rivi02 <r.verderio@itsrizzoli.it>
2022-06-15 17:52:28 +0200

    fix: fixed index.html

--- OpenDelivery/src/main/java/it/opendelivey/demo/Controllore.java
+++ OpenDelivery/src/main/java/it/opendelivey/demo/Controllore.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Controllore {
-    @GetMapping("/")
+    @GetMapping("/index")
     public String Index() {
         return "registrazione";
     }
