c11d702008fde8abd758858c3630f99270e93526
sRabosio <rabosiosimone@gmail.com>
2022-06-16 14:40:31 +0200

    feat: added indirizzo

---
+++ OpenDelivery/src/main/java/it/opendelivey/demo/dataStructures/Indirizzo.java
@@ -0,0 +1,67 @@
+package it.opendelivey.demo.dataStructures;
+
+import javax.validation.constraints.NotNull;
+
+public class Indirizzo {
+    @NotNull
+    private String tipo;
+    @NotNull
+    private String via;
+    @NotNull
+    private int cap;
+    @NotNull
+    private String civico;
+    @NotNull
+    private String città;
+    @NotNull
+    private String paese;
+
+    public Indirizzo(String via, int cap, String civico, String città, String paese) {
+        this.via = via;
+        this.cap = cap;
+        this.civico = civico;
+        this.città = città;
+        this.paese = paese;
+    }
+
+
+    public String getVia() {
+        return via;
+    }
+
+    public void setVia(String via) {
+        this.via = via;
+    }
+
+    public int getCap() {
+        return cap;
+    }
+
+    public void setCap(int cap) {
+        this.cap = cap;
+    }
+
+    public String getCivico() {
+        return civico;
+    }
+
+    public void setCivico(String civico) {
+        this.civico = civico;
+    }
+
+    public String getCittà() {
+        return città;
+    }
+
+    public void setCittà(String città) {
+        this.città = città;
+    }
+
+    public String getPaese() {
+        return paese;
+    }
+
+    public void setPaese(String paese) {
+        this.paese = paese;
+    }
+}
