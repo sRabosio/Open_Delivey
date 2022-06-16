ca7eaa2a775fa615823c8740485ad8ea2e98507b
sRabosio <rabosiosimone@gmail.com>
2022-06-16 15:17:50 +0200

    fix: added notBlank checks on attributes

--- OpenDelivery/src/main/java/it/opendelivey/demo/dataStructures/Indirizzo.java
+++ OpenDelivery/src/main/java/it/opendelivey/demo/dataStructures/Indirizzo.java
@@ -1,17 +1,23 @@
 package it.opendelivey.demo.dataStructures;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 public class Indirizzo {
     @NotNull
+    @NotBlank
     private String tipo;
     @NotNull
+    @NotBlank
     private String via;
     @NotNull
+    @NotBlank
     private int cap;
     @NotNull
+    @NotBlank
     private String civico;
     @NotNull
+    @NotBlank
     private String paese;
 
     public Indirizzo(String via, int cap, String civico, String città, String paese) {
