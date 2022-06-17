039e8b416fa9b13dd657fd397f005912c6973acf
sRabosio <rabosiosimone@gmail.com>
2022-06-17 14:31:12 +0200

    feat: added login form

--- OpenDelivery/src/main/java/it/opendelivey/demo/dataStructures/LoginForm.java
+++ OpenDelivery/src/main/java/it/opendelivey/demo/dataStructures/LoginForm.java
@@ -1,4 +1,33 @@
 package it.opendelivey.demo.dataStructures;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public class LoginForm {
+    @NotNull
+    @NotBlank
+    private String mail, password;
+
+    public LoginForm(String mail, String password) {
+        this.mail = mail;
+        this.password = password;
+    }
+
+
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
