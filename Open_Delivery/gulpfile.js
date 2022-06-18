80c76ef08c9355e9590b72f81d22f3666cb8c155
sRabosio <rabosiosimone@gmail.com>
2022-06-18 17:28:40 +0200

    feat: added sass-gulp and related tasks

--- Open_Delivery/gulpfile.js
+++ Open_Delivery/gulpfile.js
@@ -1,14 +1,40 @@
 const gulp = require("gulp");
-const sass = require("gulp-sass");
+const sass = require("gulp-sass")(require("sass"));
 const {src, series, parallel, dest, watch} = require("gulp");
 
-function style(){
+
+function commonStyle(){
     //file da convertire
-    return gulp.src("./assets/scss/**/*.scss")
+    return gulp.src("./assets/scss/common/main.scss")
 
     .pipe(sass())
 
     .pipe(gulp.dest("./assets/css"))
 }
+function homeStyle(){
+    //file da convertire
+    return gulp.src("./assets/scss/homepage/homepage.scss")
+
+    .pipe(sass())
+
+    .pipe(gulp.dest("./assets/css"))
+}
+
+function regStyle(){
+    //file da convertire
+    return gulp.src("./assets/scss/registrazione/registrazione.scss")
+
+    .pipe(sass())
+
+    .pipe(gulp.dest("./assets/css"))
+}
+
+function style(){
+    return commonStyle(),
+    homeStyle(),
+    regStyle()
+
+}
+
 
 exports.style = style;
