f4669407c444080bed909e5c0e661be421b555c6
sRabosio <rabosiosimone@gmail.com>
2022-06-28 10:27:27 +0200

    feat: added ristorante

---
+++ OpenDelivery/src/main/java/it/opendelivey/demo/dataStructures/Ristorante.java
@@ -0,0 +1,53 @@
+package it.opendelivey.demo.dataStructures;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class Ristorante {
+    @NotNull
+    @NotBlank
+    @Size(min = 3)
+    private String nome;
+
+    private Indirizzo indirizzo;
+
+    @NotNull
+    private String categorie;
+
+    @NotNull
+    private int id;
+
+    public Ristorante(String nome, int id) {
+        this.nome = nome;
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Indirizzo getIndirizzo() {
+        return indirizzo;
+    }
+
+    public void setIndirizzo(Indirizzo indirizzo) {
+        this.indirizzo = indirizzo;
+    }
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
