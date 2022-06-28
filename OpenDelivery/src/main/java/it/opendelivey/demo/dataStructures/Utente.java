10330f3478ef98393b46cdd74c4e5355bac36a83
sRabosio <rabosiosimone@gmail.com>
2022-06-28 10:13:56 +0200

    feat: added Utente

---
+++ OpenDelivery/src/main/java/it/opendelivey/demo/dataStructures/Utente.java
@@ -0,0 +1,11 @@
+package it.opendelivey.demo.dataStructures;
+
+public class Utente {
+    private String nome,
+        cognome,
+        password,
+        mail;
+    private String[] allergie;
+    private int eta;
+    private Indirizzo indirizzo;
+}
