91796d8e9090f0f89735d13e7429d51e0d2a1e6b
Rivi02 <r.verderio@itsrizzoli.it>
2022-07-06 16:03:48 +0200

    Revert "test branch commit"
    This reverts commit e556436c71062e1f575ea3a6ee58da7be2ac724c.

---
+++ OpenDelivery/src/main/java/it/opendelivey/demo/model/LoginForm.java
@@ -0,0 +1,47 @@
+package it.opendelivey.demo.model;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+public class LoginForm {
+    @NotNull
+    @NotBlank
+    private String mail;
+    @NotNull @NotBlank
+    private String password;
+
+    public LoginForm(String mail, String password) {
+        this.mail = mail;
+        this.password = password;
+    }
+
+
+    @Override
+    public String toString() {
+        return "LoginForm{" +
+                "mail='" + mail + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public static LoginForm testForm(){
+        return new LoginForm("test@tmail.com", "password");
+    }
+
+}
