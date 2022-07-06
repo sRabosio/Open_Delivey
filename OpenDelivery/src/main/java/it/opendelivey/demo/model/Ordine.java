51d75bebd7d35dbf98ff216ad57a9ac3ffa9676a
sRabosio <rabosiosimone@gmail.com>
2022-07-06 12:56:09 +0200

    feat: added ordine-utente relation

--- OpenDelivery/src/main/java/it/opendelivey/demo/model/Ordine.java
+++ OpenDelivery/src/main/java/it/opendelivey/demo/model/Ordine.java
@@ -9,6 +9,23 @@ public class Ordine {
         private Integer id;
 
         @ManyToOne
+        @JoinColumn
+        private Utente utente;
 
-        Utente utente;
+
+        public Integer getId() {
+                return id;
+        }
+
+        public void setId(Integer id) {
+                this.id = id;
+        }
+
+        public Utente getUtente() {
+                return utente;
+        }
+
+        public void setUtente(Utente utente) {
+                this.utente = utente;
+        }
 }
