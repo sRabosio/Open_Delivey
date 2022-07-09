4e2b628534014d89277524371b3fe0b39c2ffac3
sRabosio <rabosiosimone@gmail.com>
2022-07-09 10:44:59 +0200

    feat: finished profile get request

--- OpenDelivery/src/main/java/it/opendelivey/demo/controllers/ProfileController.java
+++ OpenDelivery/src/main/java/it/opendelivey/demo/controllers/ProfileController.java
@@ -1,26 +1,57 @@
 package it.opendelivey.demo.controllers;
 
+import it.opendelivey.demo.Repo.RepoIndirizzoUtente;
+import it.opendelivey.demo.Repo.RepoUtente;
+import it.opendelivey.demo.model.IndirizzoUtente;
 import it.opendelivey.demo.model.Utente;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.Set;
 
 @Controller
 public class ProfileController {
+
+    @Autowired
+    RepoUtente repoUtenteDao;
+
+    @Autowired
+    RepoIndirizzoUtente repoIndirizzoUtenteDao;
 
     @GetMapping("/profile")
     public String profile(
             Model model,
             HttpSession session
     ){
+        //prendo l'utente
+        Utente utente = (Utente) session.getAttribute("loggedUser");
+        if(!(
+                Utente.validate(utente, repoUtenteDao)
+                )) return "redirect:/login";
 
-        //TODO: controllare se i parametri sono corretti
+        //prendo i suoi indirizzi
+        ArrayList<IndirizzoUtente> indirizzi = repoIndirizzoUtenteDao.findByUtente(utente);
 
-        Utente utente = Utente.utenteSample();
         model.addAttribute(utente);
+        model.addAttribute("indirizzi", indirizzi);
 
         return "profile";
+    }
+
+    @GetMapping("/profile/changepassword")
+    public String changepassword(
+            Model model,
+            HttpSession session
+    ){
+        Utente utente = (Utente) session.getAttribute("loggedUser");
+        if(!(
+                Utente.validate(utente, repoUtenteDao)
+        )) return "redirect:/login";
+
+        return "changepassword";
     }
 }
